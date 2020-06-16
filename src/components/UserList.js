5b344f02f01c7f1b82126037fa9231cf1eb6e78d
Hadi Nsreeny <39328879+hnsreeny@users.noreply.github.com>
2020-06-16 17:52:15 +0200

    Update UserList.js

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,27 +1,7 @@
 import React from "react";
 
-const UserList = (props) => {
-  const toDos = props.list.map((item) => (
-    <li key={item.id}>
-      <input
-        type="checkbox"
-        value={item.done}
-        onChange={() => {
-          props.check(item.id);
-        }}
-      />
-      {item.title}
-      <i
-        onClick={() => {
-          console.log(item);
-          props.del(item.id);
-        }}
-      >
-        🗑
-      </i>
-    </li>
-  ));
-  return <ul>{toDos}</ul>;
+const functionalComponent = (props) => {
+  
 };
 
-export default UserList;
+export default functionalComponent;
